ef7b33f81f446d49cad4f83dfbaf087be9d8cb2c
Vinayaka Patrick Thompson <vinayaka.p.thompson@live.com>
2020-03-13 11:58:14 -0700

    added suport to send data and use numbers

--- TM1638.cpp
+++ TM1638.cpp
@@ -5,7 +5,7 @@ TM1638::TM1638(int data, int clock, int strobe){
     lKClock = clock;
     lKStrobe = strobe;
     
-    instruction = 0b10001111;
+    ins = 0b10001111;
     
     initArduino();
 }
@@ -20,20 +20,70 @@ int TM1638::initArduino(){
     digitalWrite(lKClock, HIGH);
     digitalWrite(lKStrobe, HIGH);
 
-    Serial.write("Starting Instruction write:");
     
+
+    return 0;
+}
+
+void TM1638::sendInstruction(BYTE instruction){
+   
+    digitalWrite(lKStrobe, HIGH);
     digitalWrite(lKStrobe, LOW);
-    
+
+    sendData(instruction);    
+}
+
+void TM1638::sendData(BYTE instruction){
+   
     for(int i = 0; 8 > i; i++){
-        Serial.write(lKDataIO);
         digitalWrite(lKClock, LOW);
         digitalWrite(lKDataIO, instruction[i]);
         digitalWrite(lKClock, HIGH);
     }
-    
-    digitalWrite(lKStrobe, HIGH);
 
-    return 0;
+}
+
+BYTE TM1638::sSNumber(unsigned int number){
+    //check value less than 10
+
+    BYTE result;
+    switch (number)
+    {
+    case 0:
+        result = 0b00111111;
+        break;
+    case 1:
+        result = 0b00000110;
+        break;
+    case 2:
+        result = 0b01011011;
+        break;
+    case 3:
+        result = 0b01001111;
+        break;
+    case 4:
+        result = 0b01100110;
+        break;
+    case 5:
+        result = 0b01101101;
+        break;
+    case 6:
+        result = 0b01111101;
+        break;
+    case 7:
+        result = 0b00000111;
+        break;
+    case 8:
+        result = 0b01111111;
+        break;
+    case 9:
+        result = 0b01100111;
+        break;
+    
+    default:
+        result = 0b00000000;
+    }
+    return result;
 }
 
 //destructor empty as there is nothing to destroy
--- TM1638.h
+++ TM1638.h
@@ -7,14 +7,16 @@ typedef std::bitset<8> BYTE;
 class TM1638{
     public:
         TM1638(int data, int clock, int strobe);
-
+        void sendInstruction(BYTE instruction);
+        void sendData(BYTE instruction);
+        BYTE sSNumber(unsigned int number);
         ~TM1638();
     
     private:
         int lKDataIO;
         int lKClock;
         int lKStrobe;
-        BYTE instruction;
+        BYTE ins;
 
         int initArduino();
 
